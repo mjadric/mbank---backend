2b5564d5bc51184818ef23367699751b803721ec
Mia Jadric <mjadric@pmfst.hr>
2024-04-30 19:38:06 +0200

    chgn

--- server.js
+++ server.js
@@ -13,22 +13,20 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB!');
+  console.log('Connected to Azure MongoDB!');
 });
 
 app.use(routes);
 
 const PORT = process.env.PORT || 8080;
 
-// Kreiramo HTTP server koristeći Express aplikaciju
 const server = http.createServer(app);
 
-// Slušamo na određenoj IP adresi i portu
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
